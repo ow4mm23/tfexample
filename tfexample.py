6ee2c985de977519bc53f683a85f3d876a43ed34
ow4mm23 <owm168894236@hotmail.com>
2019-03-11 14:56:18 +0800

    1456

--- tfexample.py
+++ tfexample.py
@@ -6,6 +6,7 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train/255.0, x_test/255.0
 
+#model
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(512, activation=tf.nn.relu),
@@ -19,3 +20,5 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test)
+
+
